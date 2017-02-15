24fdb05a2967d49769afb219d839f3e4dcbd682e
Patrick Burke <denormative@gmail.com>
2017-02-15 12:21:10 +0900

    stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,9 @@ module.exports = {
   },
   env: {
     browser: true,
+    es6: true,
   },
-  extends: 'airbnb-base',
+  extends: ['eslint:recommended', 'airbnb-base'],
   // required to lint *.vue files
   plugins: [
     'html'
@@ -34,6 +35,33 @@ module.exports = {
       'optionalDependencies': ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0,
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'brace-style': [ 2, "stroustrup", { "allowSingleLine": true } ],
+    'operator-linebreak': [ 2, "after" ],
+    'semi': ["error", "never"],
+    'import/no-unresolved':  ["off"], // TODO: fixme
+    'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
+    'no-use-before-define': ["error", { "functions": false }],
+    'max-len': ["warn", 132, 2, { "ignoreComments": true }],
+    'func-names': ["error", "as-needed"],
+    'no-param-reassign': ["error", { "props": false }],
+    'no-console': ["off", { allow: ["info", "warn", "error"] }],
+    'no-continue': "off",
+    'quotes': ["off", "double", { "allowTemplateLiterals": true }],
+    'no-multi-spaces': ["error", { exceptions: { "ObjectExpression": true } }],
+    'key-spacing': ["error", { align: "value" }]
+  },
+  "settings": {
+    "import/resolver": {
+      "webpack": {
+        "config": "build/webpack.base.conf.js"
+      }
+    }
+  },
 }
